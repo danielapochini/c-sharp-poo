cd513f5d113f01f95e00e7f51432062a7c420a6c
danielapochini <daniela.pochini@fatec.sp.gov.br>
2020-09-28 21:36:13 -0300

    Entrada e Saida de Dados

---
+++ EntradaSaida/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EntradaSaida
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //saida de dados
+            Console.Write("Daniela "); // imprime algo sem quebra de linha
+            Console.WriteLine("Teste"); // imprime algo na tela com quebra de linha
+
+            //entrada de dados
+            int letra = Console.Read();
+            string texto = Console.ReadLine(); //le a proxima linha de caracteres (retorna uma string)
+            Console.WriteLine(texto);  
+
+            //Console.ReadKey(false);
+            Console.ReadKey(); //capturar uma tecla digitada a partir do teclado
+        }
+    }
+}
