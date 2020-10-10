057675c1230baf21766f83be4e5e866c755c9453
danielapochini <daniela.pochini@fatec.sp.gov.br>
2020-10-09 21:40:49 -0300

    Aula Estruturas Condicionais Encadeadas (If - Else If - Else)

--- EstruturasCondicionais/Program.cs
+++ EstruturasCondicionais/Program.cs
@@ -6,15 +6,24 @@ namespace EstruturasCondicionais
     {
         static void Main(string[] args)
         {
-            int valor = 5;
+            int valor = 25;
 
-            if (valor > 5)
+            if (valor < 5)
             {
                 Console.WriteLine("Condição verdadeira");
             }
+            else if (valor >= 5 && valor < 10)
+            {
+                Console.WriteLine("Condição alternativa 1");
+            } 
+            else if (valor >=10 && valor < 20)
+            {
+                Console.WriteLine("Condição alternativa 2");
+            }
             else
             {
-                Console.WriteLine("Condição falsa");
+                Console.WriteLine("Condição alternativa final");
+
             }
             Console.ReadKey();
         }
