18d77365ecdbb3ebe51a155a7337a6f851976add
danielapochini <daniela.pochini@fatec.sp.gov.br>
2020-10-09 22:08:01 -0300

    Exercício Calculo IMC

---
+++ CalcularIMC/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace CalcularIMC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Informe o peso em kg: ");
+            double peso = double.Parse(Console.ReadLine());
+
+            Console.Write("Informe a altura em metros: ");
+            double altura = double.Parse(Console.ReadLine());
+
+            double valorIMC = peso / (altura * altura);
+
+            if (valorIMC < 20)
+            {
+                Console.WriteLine("\n IMC = " + valorIMC + " -> Abaixo do peso"); 
+            }
+            else if ((valorIMC >= 20) && (valorIMC <= 24)) {
+                Console.WriteLine("\n IMC = " + valorIMC + " -> Normal");
+            }
+            else if ((valorIMC >= 25) && (valorIMC <= 29))
+            {
+                Console.WriteLine("\n IMC = " + valorIMC + " -> Acima do peso");
+            }
+            else if ((valorIMC >= 30) && (valorIMC <= 34))
+            {
+                Console.WriteLine("\n IMC = " + valorIMC + " -> Obeso");
+            }
+            else
+            {
+                Console.WriteLine("\n IMC = " + valorIMC + " -> Muit Obeso");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
