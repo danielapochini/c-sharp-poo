6a05deeb0cdb297465951feb13b80d129a7c1e70
danielapochini <daniela.pochini@fatec.sp.gov.br>
2020-10-09 21:34:14 -0300

    Aula If - Else

---
+++ EstruturasCondicionais/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EstruturasCondicionais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor = 5;
+
+            if (valor > 5)
+            {
+                Console.WriteLine("Condição verdadeira");
+            }
+            else
+            {
+                Console.WriteLine("Condição falsa");
+            }
+            Console.ReadKey();
+        }
+    }
+}
