2b6b9cf82cb51ec9955be473c617a16243232a22
danielapochini <daniela.pochini@fatec.sp.gov.br>
2020-10-11 18:36:08 -0300

    Aula Ciclo de Repetição (While)

---
+++ EstruturasRepeticao/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace EstruturasRepeticao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor = 15;
+
+            while (valor < 100)
+            {
+                Console.WriteLine("O valor é :" + valor);
+                valor++;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
