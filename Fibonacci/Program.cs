afa51bf31c1d7f63ed90de34deb18d69686b92a3
danielapochini <daniela.pochini@fatec.sp.gov.br>
2020-10-11 18:52:50 -0300

    Exercício Fibonacci

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 0, 1, 1, 2, 3, 5, 8
+            int a = 0, b = 1, c = 0;
+
+            Console.Write("Quantos valores: ");
+            int valores = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Sequência de Fibonacci com " + valores + " valores");
+
+            for (int i = 0; i < valores; i++)
+            {
+                if (i < valores - 1)
+                {
+                    Console.Write(a + ", ");
+                }
+                else
+                {
+                    Console.Write(a);
+                }
+
+                c = a + b;
+                a = b;
+                b = c;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
