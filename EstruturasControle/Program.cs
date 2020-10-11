b2f5e4d1d4d5155d06b85b8ae5d7dfb0ddba798b
danielapochini <daniela.pochini@fatec.sp.gov.br>
2020-10-11 18:10:24 -0300

    Aula Estrutura de Controle Switch - Case

---
+++ EstruturasControle/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace EstruturasControle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int escolha = 1;
+
+            switch(escolha)
+            {
+                default:
+                    Console.WriteLine("Opção Default");
+                    break;
+                case 1:
+                    Console.WriteLine("Opção 1");
+                    break;
+                case 2:
+                    Console.WriteLine("Opção 2");
+                    break;
+                case 3:
+                case 4:
+                    Console.WriteLine("Opção Combinada");
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
