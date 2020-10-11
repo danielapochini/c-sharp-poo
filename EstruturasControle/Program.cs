a16568f415460842ae3060919dba3bcfa3cd170a
danielapochini <daniela.pochini@fatec.sp.gov.br>
2020-10-11 18:18:15 -0300

    Aula Declaração Goto

--- EstruturasControle/Program.cs
+++ EstruturasControle/Program.cs
@@ -6,6 +6,8 @@ namespace EstruturasControle
     {
         static void Main(string[] args)
         {
+            #region Switch - Case 
+            /*            
             int escolha = 1;
 
             switch(escolha)
@@ -25,6 +27,29 @@ namespace EstruturasControle
                     break;
             }
 
+            Console.ReadKey();
+            */
+            #endregion
+
+            //declaracao de um label
+            Inicio:
+            Console.Write("Escolha uma opção: ");
+            int op = int.Parse(Console.ReadLine());
+            int valor = 0;
+
+            switch (op)
+            {
+                default:
+                    goto Inicio; 
+                case 1:
+                    valor += 100;
+                    break;
+                case 2:
+                    valor += 50;
+                    goto case 1;
+            }
+
+            Console.WriteLine("Valor final: " + valor);
             Console.ReadKey();
         }
     }
