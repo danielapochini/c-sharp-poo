d449d1087d15f75f1bc97c175fee5d4b109d1b43
danielapochini <daniela.pochini@fatec.sp.gov.br>
2020-10-11 18:40:24 -0300

    Aula Estrutura de Repetição (Do - While)

--- EstruturasRepeticao/Program.cs
+++ EstruturasRepeticao/Program.cs
@@ -6,14 +6,27 @@ namespace EstruturasRepeticao
     {
         static void Main(string[] args)
         {
+            #region Ciclo While
+            /*
             int valor = 15;
 
-            while (valor < 100)
+            while (valor < 10)
             {
                 Console.WriteLine("O valor é :" + valor);
                 valor++;
             }
 
+            Console.ReadKey();
+            */
+            #endregion
+
+            int valor = 15;
+            do
+            {
+                Console.WriteLine("O valor é: " + valor);
+                valor++;
+            } while (valor < 10);
+            //irá imprimir o valor, depois irá verificar que a condição é falsa e não entrará no laço de repetição
             Console.ReadKey();
         }
     }
