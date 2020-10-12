d2cf532e7c76359f44975b840d1aca0622c1cfc0
danielapochini <daniela.pochini@fatec.sp.gov.br>
2020-10-12 12:05:24 -0300

    POO - Métodos com Parametros

---
+++ POO_MetodosClasses/Metodos.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POO_MetodosClasses
+{
+    class Metodos
+    {
+        // Métodos simples 
+        public void Cumprimentar()
+        {
+            Console.WriteLine("Olá, seja bem vindo");
+        }
+
+        // Métodos com parâmetros  
+        public void Somar(int num1, int num2)
+        {
+            int res = num1 + num2;
+            Console.WriteLine("A soma é: " + res);
+        }
+
+        public void Apresentar(string nome, int idade)
+        {
+            Console.WriteLine("Meu nome é " + nome + " e tenho " + idade + " anos");
+        }
+    }
+}
---
+++ POO_MetodosClasses/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace POO_MetodosClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Metodos m = new Metodos();
+            m.Cumprimentar();
+            m.Somar(1, 2);
+            m.Apresentar("Daniela", 26);
+        }
+    }
+}
