7509defe48570a0b469594e0928f620d1211d86d
danielapochini <daniela.pochini@fatec.sp.gov.br>
2020-10-12 11:39:51 -0300

    POO - Classes, Objetos e Atributos de Classes

---
+++ POO_ClassesObjetos/Pessoa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POO_ClassesObjetos
+{
+    class Pessoa
+    {
+        //atributos ou campos
+        public string nome;
+        public string sobrenome;
+        public int anoNascimento;
+
+
+    }
+}
---
+++ POO_ClassesObjetos/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace POO_ClassesObjetos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Classes e Objetos
+            /*
+            MinhaClasse mclasse = new MinhaClasse();
+            MinhaClasse m2 = null;
+
+            OutraClasse outra = new OutraClasse();
+            OutraClasse outra2 = outra;
+             */
+            #endregion
+
+            Pessoa p1 = new Pessoa();
+            p1.nome = "Daniela";
+            p1.sobrenome = "Pochini";
+            p1.anoNascimento = 1993;
+
+            Pessoa p2 = new Pessoa()
+            {
+                nome = "Teste01",
+                sobrenome = "Teste",
+                anoNascimento = 2020
+            };
+
+            p1.nome = "Jason";
+
+            Console.WriteLine("Pessoa 1: " + p1.nome);
+            Console.WriteLine("Pessoa 1: " + p1.sobrenome);
+            Console.WriteLine("Pessoa 1: " + p1.anoNascimento);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Pessoa 1: " + p2.nome);
+            Console.WriteLine("Pessoa 1: " + p2.sobrenome);
+            Console.WriteLine("Pessoa 1: " + p2.anoNascimento);
+
+            Console.ReadKey();
+
+        }
+
+        class MinhaClasse
+        {
+
+        }
+    }
+}
