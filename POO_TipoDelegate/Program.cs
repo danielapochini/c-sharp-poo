d5bd6d9d0690ca9eafd10697daeeea689833c9e5
danielapochini <daniela.pochini@fatec.sp.gov.br>
2020-10-13 12:02:41 -0300

    Aula Tipo Delegate

---
+++ POO_TipoDelegate/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace POO_TipoDelegate
+{
+    class Program
+    {
+
+        // delegate é um tipo de variavel quando queremos atribuir varios tipos de comportamento num único evento
+        
+        delegate void Operacao(int num1, int num2);
+
+        static void Main(string[] args)
+        {
+            Matematica m = new Matematica();
+
+            Operacao conta = null;
+
+            conta += m.Somar;
+            conta += m.Subtrair;
+            conta += m.Multiplicar;
+            conta += m.Dividir;
+            conta += m.Multiplicar;
+
+            conta(10, 2);
+
+            Console.WriteLine();
+
+            conta -= m.Subtrair;
+            conta -= m.Dividir;
+
+            conta(15, 3);
+
+            Console.ReadKey();
+        }
+    }
+}
