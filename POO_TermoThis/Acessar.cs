52c596eae87456b5a4ea294d0dc2b58668ff6c1b
danielapochini <daniela.pochini@fatec.sp.gov.br>
2020-10-13 11:46:17 -0300

    Aula Termo This

---
+++ POO_TermoThis/Acessar.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POO_TermoThis
+{
+    class Acessar
+    {
+        string senha = "abc123";
+
+        //this faz referência à classe onde ele está sendo estanciado
+
+        public bool Login(string senha)
+        {
+            return this.senha == senha;
+        }
+    }
+}
---
+++ POO_TermoThis/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace POO_TermoThis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Acessar a = new Acessar();
+
+            if (a.Login("123"))
+            {
+                Console.WriteLine("Seja bem vindo");
+            }
+            else
+            {
+                Console.WriteLine("Acesso negado!");
+            }
+        }
+    }
+}
