7ef74ce48925d1b22f6e01c207adbb5b6062ed05
danielapochini <daniela.pochini@fatec.sp.gov.br>
2020-10-13 12:12:59 -0300

    Aula Metodo Construtor

---
+++ POO_MetodoConstrutor/Pessoa.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POO_MetodoConstrutor
+{
+    class Pessoa
+    {
+        public string nome;
+        public string sobrenome;
+        public int anoNascimento;
+        public int idade;
+
+        public Pessoa()
+        {
+            nome = "Desconhecido";
+            sobrenome = "Nenhum";
+            anoNascimento = 0;
+            idade = 0;
+        }
+    }
+}
---
+++ POO_MetodoConstrutor/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace POO_MetodoConstrutor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p1 = new Pessoa();
+
+            Console.WriteLine(p1.nome);
+            Console.WriteLine(p1.sobrenome);
+            Console.WriteLine(p1.anoNascimento);
+            Console.WriteLine(p1.idade);
+             
+        }
+    }
+}
