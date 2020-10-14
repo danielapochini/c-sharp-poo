eff8c3fddb329b8b2115687e1315a6c2f2857f6a
danielapochini <daniela.pochini@fatec.sp.gov.br>
2020-10-14 15:38:52 -0300

    Aula Classes Estáticas

---
+++ POO_ClassesEstaticas/Matematica.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POO_ClassesEstaticas
+{
+    //uma classe estatica não podemos instanciar objetos, para acessar seus membros 
+    // utilizamos a própria classe como referência diretamente
+    // ficará visível à todo projeto.
+    static class Matematica
+    {
+        public static int taxa;
+
+        public static int Adicionar(int valor)
+        {
+            return valor + taxa;
+        }
+
+        public static int Diminuir(int valor)
+        {
+            return valor - taxa;
+        }
+
+    }
+}
---
+++ POO_ClassesEstaticas/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace POO_ClassesEstaticas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Matematica.taxa = 10;
+
+            int valor1 = Matematica.Adicionar(200);
+            int valor2 = Matematica.Adicionar(200);
+
+            Console.WriteLine("Valor 1: " + valor1);
+            Console.WriteLine("Valor 2: " + valor2);
+        }
+    }
+}
