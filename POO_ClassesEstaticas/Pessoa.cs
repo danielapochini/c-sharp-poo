b5233b46eb460e5e215054160b4518574670395b
danielapochini <daniela.pochini@fatec.sp.gov.br>
2020-10-14 16:08:26 -0300

    Aula Membros Estáticos

---
+++ POO_ClassesEstaticas/Pessoa.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POO_ClassesEstaticas
+{
+    class Pessoa
+    {
+        // membros estaticos são acessados diretamente pela refêrencia da classe 
+        public static int maioridade = 18;
+         
+        // membros comuns o acesso é realizado apenas pelos objetos instanciados
+        public string nome;
+        public int idade;
+
+        public void Apresentar()
+        {
+            Console.WriteLine("Olá, eu sou " + nome);
+        }
+
+        public static int CalcularIdade(int anoNascimento)
+        {
+            return DateTime.Now.Year - anoNascimento;
+        }
+
+    }
+}
--- POO_ClassesEstaticas/Program.cs
+++ POO_ClassesEstaticas/Program.cs
@@ -6,6 +6,8 @@ namespace POO_ClassesEstaticas
     {
         static void Main(string[] args)
         {
+            #region Classe Estatica
+            /*
             Matematica.taxa = 10;
 
             int valor1 = Matematica.Adicionar(200);
@@ -13,6 +15,21 @@ namespace POO_ClassesEstaticas
 
             Console.WriteLine("Valor 1: " + valor1);
             Console.WriteLine("Valor 2: " + valor2);
+            */
+            #endregion
+
+            #region Membros estáticos
+
+            Pessoa.maioridade = 21;
+
+            Pessoa p = new Pessoa();
+            p.nome = "Daniela";
+            p.idade = Pessoa.CalcularIdade(1993);
+
+            Console.WriteLine(p.nome);
+            Console.WriteLine(p.idade);
+            Console.WriteLine(Pessoa.maioridade);
+            #endregion
         }
     }
 }
