6f49a8558cfee1144b22ad07988bb4e0fc368094
danielapochini <daniela.pochini@fatec.sp.gov.br>
2020-10-14 16:17:42 -0300

    Classes parciais

---
+++ POO_ClassesEstaticas/PessoaAtributos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POO_ClassesEstaticas
+{
+    partial class Pessoa
+    {
+        // membros estaticos são acessados diretamente pela refêrencia da classe 
+        public static int maioridade = 18;
+         
+        // membros comuns o acesso é realizado apenas pelos objetos instanciados
+        public string nome;
+        public int idade;
+         
+    }
+}
--- POO_ClassesEstaticas/PessoaMetodos.cs
+++ POO_ClassesEstaticas/PessoaMetodos.cs
@@ -4,15 +4,8 @@ using System.Text;
 
 namespace POO_ClassesEstaticas
 {
-    class Pessoa
+    partial class Pessoa
     {
-        // membros estaticos são acessados diretamente pela refêrencia da classe 
-        public static int maioridade = 18;
-         
-        // membros comuns o acesso é realizado apenas pelos objetos instanciados
-        public string nome;
-        public int idade;
-
         public void Apresentar()
         {
             Console.WriteLine("Olá, eu sou " + nome);
@@ -22,6 +15,5 @@ namespace POO_ClassesEstaticas
         {
             return DateTime.Now.Year - anoNascimento;
         }
-
     }
 }
