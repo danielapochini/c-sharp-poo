415e2ff660cdb056d4da864d8cec38f6b94b46a2
danielapochini <daniela.pochini@fatec.sp.gov.br>
2020-10-15 14:52:37 -0300

    Aula Membros Sealed

---
+++ POO_Heranca/Classes.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POO_Heranca
+{
+    class Humano
+    {
+        // A palavra-chave virtual é usada para modificar uma declaração de método, propriedade, indexador ou evento 
+        // e permitir que ela seja substituída em uma classe derivada.
+        public virtual void Olhos()
+        {
+            Console.WriteLine("Humano.Olhos");
+        }
+
+        public virtual void Cabelos()
+        {
+            Console.WriteLine("Humano.Cabelos");
+        }
+    }
+
+    class Pessoa : Humano
+    {
+        // override: está sobrescrevendo o método da classe que foi herdada
+        // sealed: não permitira que o método seja modificado
+        public sealed override void Olhos()
+        {
+            Console.WriteLine("Pessoa.Olhos");
+        }
+
+        public override void Cabelos()
+        {
+            Console.WriteLine("Pessoa.Cabelos");
+        }
+    }
+
+    class Homem : Pessoa
+    { 
+        /*        
+        public override void Olhos()
+        {
+            Console.WriteLine("Homem.Olhos");
+        }
+        */
+
+        public override void Cabelos()
+        {
+            Console.WriteLine("Homem.Cabelos");
+        }
+    }
+}
--- POO_Heranca/Program.cs
+++ POO_Heranca/Program.cs
@@ -6,6 +6,8 @@ namespace POO_Heranca
     {
         static void Main(string[] args)
         {
+            #region Parte 1
+            /*          
             Carro c = new Carro();
             Bicicleta b = new Bicicleta();
             //Veiculo v = new Veiculo(); não é possível instanciar objetos
@@ -15,6 +17,25 @@ namespace POO_Heranca
 
             c.Parar();
             b.Parar();
+            */
+            #endregion
+
+            Humano a = new Humano();
+            Pessoa b = new Pessoa();
+            Homem c = new Homem();
+
+            Console.WriteLine("Humano");
+            a.Olhos();
+            a.Cabelos();
+
+            Console.WriteLine("\nPessoa");
+            b.Olhos();
+            b.Cabelos();
+
+            Console.WriteLine("\nHomem");
+            c.Olhos();
+            c.Cabelos();
+
         }
     }
 }
