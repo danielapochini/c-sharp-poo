d551efb0aa27d491d94353f247f6713a4dd9f749
danielapochini <daniela.pochini@fatec.sp.gov.br>
2020-10-15 12:13:33 -0300

    Aula Encapsulamento

---
+++ POO_Encapsulamento/Conta.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POO_Encapsulamento
+{
+    class Conta
+    {
+
+        // encapsulamento é o processo de ocultar ou esconder os membros de uma classe
+        // dando acesso exterior usando um modificador de acesso e o encapsulamento fornece
+        // uma maneira de preservar a integridade do estado dos dados
+
+        public string _cliente;
+        public double _saldo;
+        private double _taxa = 10;
+
+        public string Cliente
+        {
+            get { return _cliente;  }
+            set
+            {
+                if (value != "Daniela" && value != "Teste")
+                {
+                    _cliente = "Visitante";
+                    Saldo = 0;
+                }
+                else
+                {
+                    _cliente = value;
+                    Saldo = 100;
+                }
+            }
+        }
+
+        public double Saldo
+        {
+            get { return _saldo; }
+
+            private set { _saldo = value; }
+        }
+
+        public void Sacar(double valor)
+        {
+            valor += _taxa;
+            _saldo -= valor;
+        }
+
+        public void Depositar(double valor)
+        {
+            _saldo += valor;
+        }
+    }
+}
---
+++ POO_Encapsulamento/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace POO_Encapsulamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Conta c = new Conta();
+
+            c.Cliente = "Daniela";
+            //c.Saldo = 100;
+
+            // Operação de Deposito
+            double valor = 100;
+            c.Depositar(valor);
+
+            //Operação de Saque
+            double valor2 = 50;
+            c.Sacar(valor2);
+             
+            //Resultado Salto Atual
+            Console.WriteLine("Cliente: " + c.Cliente);
+            Console.WriteLine("Saldo: " + c.Saldo);
+        }
+    }
+}
