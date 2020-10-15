5fd7150a72e7cf0eb00c673359029c8a411b7acd
danielapochini <daniela.pochini@fatec.sp.gov.br>
2020-10-15 15:34:55 -0300

    Aula Polimorfismo

---
+++ POO_Polimorfismo/Classes.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POO_Polimorfismo
+{
+    //polimorfismo: todos possuem o mesmo metodo, mas conforme o tipo, ele assume uma forma diferente.
+    abstract public class Forma
+    {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+
+        public int Altura { get; set; }
+        public int Largura { get; set; }
+
+        public int Raio { get; set; }
+
+        public virtual void Desenhar()
+        {
+            Console.WriteLine("Preparando-se para desenhar");
+        }
+
+        public virtual void Area()
+        {
+            int area = Largura * Altura;
+            Console.WriteLine("Area " + area);
+        }
+    }
+
+   public class Circulo : Forma
+    {
+        public override void Desenhar()
+        {
+            Console.WriteLine("Desenhando um Círculo");
+            base.Desenhar();
+        }
+
+        public override void Area()
+        {
+            double area = 3.14 * (Raio * Raio);
+            Console.WriteLine("Area Círculo " + area);
+        }
+    }
+
+    public class Retangulo : Forma
+    {
+        public override void Desenhar()
+        {
+            Console.WriteLine("Desenhando um Retangulo"); 
+        }
+
+        public override void Area()
+        {
+            base.Area();
+        }
+    }
+
+    public class Triangulo : Forma
+    {
+        public override void Desenhar()
+        {
+            Console.WriteLine("Desenhando um Triangulo");
+            base.Desenhar();
+        }
+
+        public override void Area()
+        {
+            base.Area();
+            int area = (Largura * Altura) / 2;
+            Console.WriteLine("Area Retangulo " + area);
+        }
+    }
+}
---
+++ POO_Polimorfismo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace POO_Polimorfismo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Forma a = new Forma();
+            Forma b = new Triangulo(); // Triangulo é uma forma, segundo o polimorfismo
+            Forma c = new Circulo();
+            Forma d = new Retangulo();
+
+            //Console.WriteLine("Forma");
+            //a.Desenhar();
+            Console.WriteLine("Triangulo");
+            b.Desenhar();
+            Console.WriteLine("Circulo");
+            c.Desenhar();
+            Console.WriteLine("Retangulo");
+            d.Desenhar();
+        }
+    }
+}
