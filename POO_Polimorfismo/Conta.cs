cbd39dfe87be936ad15209bc4defa9b3a4f681f3
danielapochini <daniela.pochini@fatec.sp.gov.br>
2020-10-15 15:48:48 -0300

    Aula Interfaces

---
+++ POO_Polimorfismo/Conta.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POO_Polimorfismo
+{
+    class CaixaEletronico : IConta
+    {
+        public string Usuario { get; set; }
+
+        public void Depositar()
+        {
+             
+        }
+
+        public void Sacar()
+        {
+             
+        }
+
+        public void Saldo()
+        {
+             
+        }
+
+        public void SolicitarEmprestimo()
+        {
+
+        }
+    }
+
+    //interface inicia-se com I 
+    // todos os membros de uma interface são public, para serem acessíveis a quem herdar
+    // todos os metodos são abstratos, sem precisar colocar o modificador
+    interface IConta
+    {
+        string Usuario { get; set; }
+        void Depositar();
+
+        void Sacar();
+
+        void Saldo();
+
+    }
+}
