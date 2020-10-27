c375feb57d75a10dee83771a20ddb5ab31d2191a
danielapochini <daniela.pochini@fatec.sp.gov.br>
2020-10-27 20:43:53 -0300

    Aula Recursividade

---
+++ POO_Recursividade/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace POO_Recursividade
+{
+    // recursividade: um recurso de um método chamar a si mesmo
+    // ou seja, dentro do seu corpo de execução, ele executa uma subrotina dele mesmo.
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Recursiva r = new Recursiva();
+            //r.Executar("Ola mundo!", 5); 
+            r.ExecutarRecursivo("Ola mundo!", 5);
+        }
+    }
+}
---
+++ POO_Recursividade/Recursiva.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POO_Recursividade
+{
+    class Recursiva
+    {
+        // forma não recursiva
+        public void Executar(String mensagem, int n)
+        {
+            for(int i=0; i<n; i++)
+            {
+                Console.WriteLine(mensagem);
+            }
+
+        }
+
+        // forma recursiva
+        public void ExecutarRecursivo(String mensagem, int n)
+        {
+            if (n > 0)
+            {
+                Console.WriteLine(mensagem);
+                ExecutarRecursivo(mensagem, n - 1); 
+            }
+        }
+    }
+}
