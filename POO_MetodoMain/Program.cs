e71b8fa1a5f82a2b14d9650a23a81dc2698ec108
danielapochini <daniela.pochini@fatec.sp.gov.br>
2020-10-27 21:01:16 -0300

    Aula Argumentos Metodo Main

---
+++ POO_MetodoMain/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace POO_MetodoMain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string nome = args[0];
+            string senha = "abc123";
+
+            if ( senha != args[1])
+            {
+                Console.WriteLine("Acesso Negado");
+            }
+            else
+            {
+                Console.WriteLine("Olá " + args[0]);
+
+            }
+
+        }
+    }
+}
